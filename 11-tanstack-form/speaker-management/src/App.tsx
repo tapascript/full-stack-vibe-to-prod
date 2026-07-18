2f6511ec1a70eca0e6f288b00411380b9fac40d1
Tapas Adhikary <tapas.adhikary@gmail.com>
2026-07-18 15:12:28 +0530

    (feat) Added other basic forms

--- 11-tanstack-form/speaker-management/src/App.tsx
+++ 11-tanstack-form/speaker-management/src/App.tsx
@@ -1,10 +1,15 @@
 import { SpeakerForm } from "./components/SpeakerForm";
+
+import { RefRegistrationForm } from "./components/RefRegistrationForm";
+import { StandardRegistrationForm } from "./components/StandardRegistrationForm";
 function App() {
-  return (
-    <div className="flex flex-col items-center">
-      <SpeakerForm />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center">
+            <StandardRegistrationForm />
+            <RefRegistrationForm />
+            <SpeakerForm />
+        </div>
+    );
 }
 
 export default App;
